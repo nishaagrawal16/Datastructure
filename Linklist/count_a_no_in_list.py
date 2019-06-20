6f7f1e26d9be3f96c74a2365f8cb5d305fe91ce3
Nisha Agrawal <nishaa@google.com>
2019-06-20 16:06:13 +0530

    given values from user

--- Linklist/count_a_no_in_list.py
+++ Linklist/count_a_no_in_list.py
@@ -1,4 +1,6 @@
-# Count A number is present how many times in the link list
+#!/usr/bin/python
+
+# Count a number is present how many times in the link list.
 class Node:
   def __init__(self, value):
     self.info = value
@@ -8,28 +10,25 @@ class LinkList:
   def __init__(self):
     self.start = None
 
-  def create_list(self):
-    n1 = Node(10)
-    n2 = Node(20)
-    n3 = Node(10)
-    n4 = Node(40)
-    n5 = Node(10)
-    n6 = Node(10)
-    n1.next = n2
-    n2.next = n3
-    n3.next = n4
-    n4.next = n5
-    n5.next = n6
-    self.start = n1
+  def create_list(self, li):
+    if self.start is None:
+      self.start = Node(li[0])
+    p = self.start
+    i = 1
+    while i < len(li):
+      temp = Node(li[i])
+      p.next = temp
+      p = p.next
+      i = i + 1
 
   def traverse(self):
     p = self.start
     i = 0
     while p is not None and i<10:
-      print(p.info, end=" ")
+      print('%d ->' % p.info, end=" ")
       p = p.next
       i = i + 1
-    print('\n')
+    print('None')
 
   def countANumber(self, num):
     count = 0
@@ -42,8 +41,33 @@ class LinkList:
         count = count + 1
       p = p.next
     return count
-linkList = LinkList()
-linkList.create_list()
-linkList.traverse()
-count = linkList.countANumber(10)
-print('Number of times value %s present in list: %s' % (10, count))
+
+def main():
+  num = int(input('Enter number of Nodes in the link list: '))
+  link_list = []
+  i = 0
+  while i < num:
+    ele = int(input('Enter %d element: ' % i))
+    link_list.append(ele)
+    i = i + 1
+
+  linkList = LinkList()
+  linkList.create_list(link_list)
+  linkList.traverse()
+  count_num = int(input('Enter a number: '))
+  count = linkList.countANumber(count_num)
+  print('Number of times value %s present in list: %s' % (count_num, count))
+
+if __name__ == '__main__':
+  main()
+
+# Output:
+# -------
+# Enter number of Nodes in the link list: 4
+# Enter 0 element: 1
+# Enter 1 element: 3
+# Enter 2 element: 2
+# Enter 3 element: 1
+# 1 -> 3 -> 2 -> 1 -> None
+# Enter a number: 1
+# Number of times value 1 present in list: 2
