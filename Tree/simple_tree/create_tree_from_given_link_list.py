b2148dc509502d8c5af3a036f2c75787c7f5f4f6
nishaagrawal16 <nisha.agrawal1@gmail.com>
2019-11-28 14:46:16 +0530

    create tree ans traversal right view

---
+++ Tree/simple_tree/create_tree_from_given_link_list.py
@@ -0,0 +1,88 @@
+# Construct Complete Binary Tree from its Linked List Representation
+# 1. Create an empty queue.
+# 2. Make the first node of the list as root, and enqueue it to the queue.
+# 3. Until we reach the end of the list, do the following.
+#   a. Dequeue one node from the queue. This is the current parent.
+#   b. Traverse two nodes in the list, add them as children of the current parent.
+#   c. Enqueue the two nodes into the queue.
+# O(n)
+class Node:
+  def __init__(self, value):
+    self.info = value
+    self.next = None
+
+class SingleLinkList:
+  def __init__(self):
+    self.start = None
+
+  def create_list(self):
+    n1 = Node(10)
+    n2 = Node(20)
+    n3 = Node(30)
+    n4 = Node(40)
+    n5 = Node(50)
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    self.start = n1
+
+  def traverse(self):
+    p = self.start
+    while p is not None:
+      print p.info,
+      p = p.next
+    print('None')
+
+
+class TreeNode:
+  def __init__(self, value):
+    self.data = value
+    self.left = None
+    self.right = None
+
+class Tree:
+  def __init__(self):
+    self.root = None
+
+  def treeTraversalInOrder(self, root):    
+    if root:
+      self.treeTraversalInOrder(root.left)
+      print root.data,
+      self.treeTraversalInOrder(root.right)
+
+def create_tree(start):
+  que = []
+  current = start
+  t = Tree()
+  t.root = TreeNode(current.info)
+  que.append(t.root)
+  current = current.next
+  while current is not None:
+    parent = que.pop(0)
+    left_child = None
+    right_child = None
+    left_child = TreeNode(current.info)
+    que.append(left_child)
+    current = current.next
+    if current is not None:      
+      right_child = TreeNode(current.info)
+      que.append(right_child)
+      current = current.next      
+    parent.left = left_child
+    parent.right = right_child
+    
+  print('*********** TRAVERSE Tree ***************') 
+  t.treeTraversalInOrder(t.root)
+
+def main():
+  link_list = SingleLinkList()
+  print('*********** CREATING LIST ***************')
+  link_list.create_list()
+  print('*********** TRAVERSE LIST ***************')
+  link_list.traverse()
+  create_tree(link_list.start)
+  
+
+if __name__ == '__main__':
+  main()
---
+++ Tree/simple_tree/right_view_of_a_binary_tree.py
@@ -0,0 +1,61 @@
+# Print Right View of a Binary Tree
+# https://www.geeksforgeeks.org/print-right-view-binary-tree-2/
+# O(n)
+class Node:
+  def __init__(self, value):
+    self.info = value
+    self.left = None
+    self.right = None
+
+class Tree:
+  def __init__(self):
+    self.root = None
+
+  def createTree(self):
+    n1 = Node(10)
+    n2 = Node(20)
+    n3 = Node(30)
+    n4 = Node(40)
+    n5 = Node(50)
+    self.root = n1
+    n1.left = n2
+    n1.right = n3
+    n2.left = n4
+    n2.right = n5
+
+  def treeTraversalInOrder(self, root):
+    if root:
+      self.treeTraversalInOrder(root.left)
+      print root.info,
+      self.treeTraversalInOrder(root.right)
+
+  def viewRight(self, root):
+    max_level = [0]
+    self.viewRightUtil(root, 1, max_level)
+
+  def viewRightUtil(self, root, level, max_level):
+    if root is None:
+      return
+    
+    if max_level[0] < level:
+      print root.info, 
+      max_level[0] = level
+
+    self.viewRightUtil(root.right, level + 1, max_level)
+    self.viewRightUtil(root.left, level + 1, max_level)
+
+
+def main():
+  print('***************** TREE ******************\n')
+  t = Tree()
+  t.createTree()
+  print('********** INORDER TRAVERSAL ************')
+  t.treeTraversalInOrder(t.root)
+  print('\n************* RIGHT VIEW ***************')
+  t.viewRight(t.root)
+  
+
+
+
+if __name__ == '__main__':
+  main()
