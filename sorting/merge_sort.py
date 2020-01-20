203d044d2577391cc5396ad0bbed38d3decc6425
nishaagrawal16 <nisha.agrawal1@gmail.com>
2020-01-20 15:05:17 +0530

    new sortings

---
+++ sorting/merge_sort.py
@@ -0,0 +1,71 @@
+# Date: 20-Jan-2020
+# https://www.geeksforgeeks.org/python-program-for-merge-sort/
+
+# Merge Sort is a Divide and Conquer algorithm.
+# It divides input array in two halves, calls itself for the two halves and
+# then merges the two sorted halves. The merge() function is used for merging two halves. 
+# O(nlogn)
+
+def merge(un_list, l, m, r):
+  n1 = m - l + 1
+  n2 = r - m
+
+  # creats the temporary lists.
+  L = [0]*n1
+  R = [0]*n2
+
+  # Copy data to temp arrays L[] and R[] 
+  for i in range(n1):
+    L[i] = un_list[l + i]
+  for j in range(n2):
+    R[j] = un_list[m + 1 + j]
+
+  i = 0
+  j = 0
+  k = l
+
+  while i < n1 and j < n2:
+    if L[i] <= R[j]:
+      un_list[k] = L[i]
+      i = i + 1
+    else:
+      un_list[k] = R[j]
+      j = j + 1
+    k = k + 1
+
+  if i < n1:
+    un_list[k] = L[i]
+    i = i + 1
+    k = k + 1
+
+  if j < n2:
+    un_list[k] = R[j]
+    j = j + 1
+    k = k + 1
+
+def merge_sort(un_list, l, r):
+  if (l < r):
+    m = (l + (r-1))/2 # (left + (right -1))/2
+    merge_sort(un_list, l, m)
+    merge_sort(un_list, m + 1, r)
+    merge(un_list, l, m, r)
+
+def main():
+  unsorted_list = [8, 5, 6, 9, 1, 4, 10, 3, 2, 7]
+  print('************ UNSORTED LIST **************')
+  print(unsorted_list)
+  n = len(unsorted_list)
+  merge_sort(unsorted_list, 0, n-1)
+  print('************** SORTED LIST **************')
+  print(unsorted_list)
+
+
+if __name__ == '__main__':
+  main()
+
+# Output:
+# -------
+# ************ UNSORTED LIST **************
+# [8, 5, 6, 9, 1, 4, 10, 3, 2, 7]
+# ************** SORTED LIST **************
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
---
+++ sorting/quick_sort.py
@@ -0,0 +1,49 @@
+# Date: 20-Jan-2020
+# https://www.geeksforgeeks.org/python-program-for-quicksort/#
+
+# In quick sort, Target of partitions is, given an array and
+# an element x of array as pivot, put x at its correct position
+# in sorted array and put all smaller elements (smaller than x) before x,
+# and put all greater elements (greater than x) after x. 
+# All this should be done in linear time.
+# O(nlogn)
+
+def partition(un_list, low, high):
+  i = low - 1
+
+  # Select the last element as pivot.
+  pivot = un_list[high]
+  for j in range(low, high):
+    # Elements wich are less than pivot will placed from starting of the list.
+    if un_list[j] <= pivot:
+      i = i + 1
+      un_list[j], un_list[i] = un_list[i], un_list[j]
+  # Set pivot after the smaller sequence.
+  un_list[i+1], un_list[high] = un_list[high], un_list[i+1]
+  return i+1
+
+def quick_sort(un_list, low, high):
+  if (low < high):
+    pi = partition(un_list, low, high)
+    quick_sort(un_list, low, pi-1)
+    quick_sort(un_list, pi+1, high)
+
+def main():
+  unsorted_list = [8, 5, 6, 9, 1, 4, 10, 3, 2, 7]
+  print('************ UNSORTED LIST **************')
+  print(unsorted_list)
+  n = len(unsorted_list)
+  quick_sort(unsorted_list, 0, n-1)
+  print('************** SORTED LIST **************')
+  print(unsorted_list)
+
+
+if __name__ == '__main__':
+  main()
+
+# Output:
+# -------
+# ************ UNSORTED LIST **************
+# [8, 5, 6, 9, 1, 4, 10, 3, 2, 7]
+# ************** SORTED LIST **************
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
