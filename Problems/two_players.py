1ae042d692f8a0a495995f2e9f6e1baf475bef6a
nishaagrawal16 <nisha.agrawal1@gmail.com>
2020-01-22 15:47:31 +0530

    problems

--- Problems/two_players.py
+++ Problems/two_players.py
@@ -1,12 +1,15 @@
-# ****************************************************
-# There are two players which are playing a game in which each player has assign a character.
-# He needs to remove the character and get the hightest score.
-# Suppose P1 has 'a' and P2 has 'x'
+# ****************************************************************************
+# There are two players which are playing a game in which each player has 
+# assign a character. He needs to remove the character which are more than
+# one in a adjacent sequence and get the hightest score. Suppose P1 has 'a'
+# and P2 has 'x'. P1 needs to delete aa and aaa and save only one character.
+# Simlarly P2 needs to do this for x character.
 # O(n)
-# ****************************************************
+# ****************************************************************************
 
 
 class Solution(object):
+  # O(n2)
   def count_max_score(self, str1):
     p1_score = 0
     p2_score = 0
@@ -27,6 +30,8 @@ class Solution(object):
           j = j + 1
         if count != 0:
            p1_score = p1_score + 1
+
+      # Needs to break here otherwise it will become the infinite loop.
       if (j == len(str1)):
         break
 
@@ -44,6 +49,7 @@ class Solution(object):
           p2_score = p2_score + 1
     print('P1 score= {} \nP2 score= {}'.format(p1_score, p2_score))  
 
+  # O(n)
   def count_max_score_optimize(self, str1):
     p1_score = 0
     p2_score = 0
