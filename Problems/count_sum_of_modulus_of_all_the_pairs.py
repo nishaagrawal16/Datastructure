28f4be31013b85ba28f3407b0b5562d959f2d78f
nishaagrawal16 <51795688+nishaagrawal16@users.noreply.github.com>
2021-08-25 22:09:57 +0530

    Update count_sum_of_modulus_of_all_the_pairs.py
    Added time complexity

--- Problems/count_sum_of_modulus_of_all_the_pairs.py
+++ Problems/count_sum_of_modulus_of_all_the_pairs.py
@@ -5,6 +5,7 @@
 # Output: 5
 # (1 % 1) + (1 % 2) + (1 % 3) + (2 % 1) + (2 % 2)
 # + (2 % 3) + (3 % 1) + (3 % 2) + (3 % 3) = 5
+# O(k^2) where k is the hightest number
 
 class Solutions:
   def sum_of_mod(self, li):
