c56a61a3bb14c373b3c41dacb717579b2ad7763e
nishaagrawal16 <51795688+nishaagrawal16@users.noreply.github.com>
2021-11-12 00:36:38 +0530

    Create remove_duplicates_in_sorted_array.py
    Added a new program

---
+++ Problems/remove_duplicates_in_sorted_array.py
@@ -0,0 +1,22 @@
+# Remove duplicate elements from sorted array
+# Time Complexity O(n)
+# Space complexity O(1)
+
+class Solution:
+    def removeDuplicates(self, nums):
+        # Maintain a index for counting and placing the distinct element.
+        i = 0
+        for j in range(1, len(nums)):
+            # If elements are not equal, place the unique value to the next position.
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        i += 1
+        return i
+
+s = Solution()
+n = [1, 2, 2, 3, 3, 3, 4]
+print('Initial list: ', n)
+new_length = s.removeDuplicates(n)
+print('total distinct elements in the list: ', new_length)
+print('Inplace modified list:', n)
