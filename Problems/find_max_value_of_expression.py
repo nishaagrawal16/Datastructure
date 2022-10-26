b1e9e209584a06fbd8a794d36ab6e8c1ac6b1278
nishaagrawal16 <51795688+nishaagrawal16@users.noreply.github.com>
2022-10-27 01:11:54 +0530

    Use multi variable assignment for initializing all variables

--- Problems/find_max_value_of_expression.py
+++ Problems/find_max_value_of_expression.py
@@ -8,10 +8,8 @@
 
 class Solutions:
     def find_max(self, arr):
-        min1 = arr[0] # for (arr[i] + i)
-        max1 = arr[0]
-        min2 = arr[0] # for (arr[i] - i)
-        max2 = arr[0]
+        # min1 is for `(arr[i] + i)` and min2 is for `(arr[i] - i)`
+        min1 = max1 = min2 = max2 = arr[0]
         i = 1
         while i < len(arr):
             # For getting the min and max in arr[i] + i
