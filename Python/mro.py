12aa9a1f1989b9f38df25fc9af284bd85d8cbf16
Nisha Agrawal <nishaa@google.com>
2022-12-17 18:27:21 +0000

    doc path changed

--- Python/mro.py
+++ Python/mro.py
@@ -1,6 +1,5 @@
-# Method Resolution Order(MRO) 
-# https://makina-corpus.com/blog/metier/2014/python-tutorial-understanding-python-mro-class-search-path
-# In MRO we have two concepts:
+# Method Resolution Order(MRO)
+# https://makina-corpus.com/python/python-tutorial-understanding-python-mro-class-search-path
 # 1) Old style: when object is not the parent class as we are using python2.
 #               We follow the DLR or depth-first left to right In which we first go
 #               to the parent and grand parent if available than left to right.
@@ -14,15 +13,15 @@
 class X:
     def who_am_i(self):
         print("I am a X")
-    
+
 class Y:
     def who_am_i(self):
         print("I am a Y")
-    
+
 class A(X, Y):
     def who_am_i(self):
         print("I am a A")
-    
+
 class B(Y, X):
      def who_am_i(self):
          print("I am a B")
@@ -45,15 +44,15 @@ f.who_am_i()
 class X(object):
     def who_am_i(self):
         print("I am a X")
-    
+
 class Y(object):
     def who_am_i(self):
         print("I am a Y")
-    
+
 class A(X, Y):
     def who_am_i(self):
         print("I am a A")
-    
+
 class B(Y, X):
      def who_am_i(self):
          print("I am a B")
